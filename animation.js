b36571ca8bb9856461f768859745fbb0ffeb7c38
Kazice <1214673464@qq.com>
2018-02-18 14:18:07 +0800

    相关文件

---
+++ animation.js
@@ -0,0 +1,10 @@
+function byId(id) {
+    return typeof(id) === string ? document.getElementById(id) : id;
+}
+
+var img1 = byid("img1"),img2 = byid("img2"),img3 = byid("img3"),img4 = byid("img4");
+var img5 = byid("img5"),img6 = byid("img6"),img7 = byid("img7"),img8 = byid("img8");
+
+function greater(){
+    
+}
